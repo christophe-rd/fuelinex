c61ebc3ae0e00d8a55f8780a9b2930bd6428f28e
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-02-06 17:43:50 -0800

    cleaning measurements

--- analyses/rcode/cleaning/cleaningMeasurements.R
+++ analyses/rcode/cleaning/cleaningMeasurements.R
@@ -15,13 +15,13 @@ if(length(grep("christophe", getwd()) > 0)) {
 } else if(length(grep("christophe", getwd())) > 0){
   setwd("/Users/christophe_rouleau-desrochers/github/fuelinex/analyses/")
 } 
-
+list.files()
 # read it
 d<-read.csv("input/TreeMeasurements.csv", header=TRUE)
 str(d)
 # start small
 dcut <- d[1:180, c("tree_ID","species", "heightWinter24", "heightWinter25")]
-dput(dcut)
+
 # Start by looking at height increment!
 g <- ggplot(d, aes(x = Year, y = Height, fill = Species)) +
   geom_boxplot() 
@@ -30,8 +30,8 @@ g <- ggplot(d, aes(x = Year, y = Height, fill = Species)) +
 d24<-d[,c("tree_ID", "species", "heightWinter24", "heightWinter24_trunk2", "diameterWinter24", "diameterWinter24_trunk2", "noteWinter24")]
 
 # add year
-
 d24$year <- 2024
+d24$season <- "winter"
 
 # Usage example
 new_data <- reshape_data(your_dataframe)
