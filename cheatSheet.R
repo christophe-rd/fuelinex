c423dddf6e8fdc2b30a54cbf0afec93a2ed1bd18
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-05-23 19:05:49 -0700

    new functions to cheatsheet

--- cheatSheet.R
+++ cheatSheet.R
@@ -8,3 +8,6 @@ phenos<-phenos%>%tidyr::spread(phase, doy)
 
 ### Count the number of time each id is present in a df
 nbobsperID <- d %>% count(plantNickname)
+
+### Extracting Characters from a specific position
+cg18$species<-substr(cg18$Ind, 0,6) # here $species will return the characters from the position 0 to 6
