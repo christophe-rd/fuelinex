37c46ab05fd1f4bbf13cbbdfaa8e9b5653405bb9
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-05-30 10:53:47 -0700

    finished cleaning senescence observations

--- analyses/cleaning/source/2024cleaningSenescence.R
+++ analyses/cleaning/source/2024cleaningSenescence.R
@@ -54,9 +54,11 @@ notes_list <- lapply(sub$Notes, extract_notes)
 notes_list <- lapply(notes_list, function(x) if (is.null(x)) data.frame(DOY = NA, Note = NA, stringsAsFactors = FALSE) else x)
 # Combine into a single data frame
 notes_df <- do.call(rbind, notes_list)
-
-
 notes_df$tree_ID <- rep(sub$tree_ID, sapply(notes_list, function(x) nrow(x)))
+
+### === === === === === === === === === === === === ###
+#### Get a column for leaf drop and greeness loss ####
+### === === === === === === === === === === === === ###
 # subset only for comments about loss of greeness or lost leaves
 unique(notes_df$Note)
 vec <- c("no green leaves", "last leaves fell", "cant measure chl", "no leaves")
@@ -76,12 +78,84 @@ leafdrop <- subby %>%
     values_from = DOY
   )
 
+### === === === === === === === === === ###
+#### Organize chl and green leaf cover ####
+### === === === === === === === === === ###
 
-# === === == === === === === === == === === === === === == === === ===
-# === === == === === === === === == === === === === === == === === ===
+### === === === === === ###
+##### Start with notes #####
+### === === === === === ###
+# get notes df and remove last leaves fell and no green leaves becasue they've been adressed separately
+notes_df_upd <- subset(notes_df, !(Note %in% c("no green leaves", "last leaves fell")))
+
+# remove rows that don't have notes
+noteslong <- notes_df_upd[!is.na(notes_df_upd$DOY) & notes_df_upd$DOY != "", ]
+# paste ID and DOY
+noteslong$ID_DOY <- paste(noteslong$tree_ID, noteslong$DOY, sep = "_")
+# keep only 2 col
+noteslong <- noteslong[, c("ID_DOY", "Note")]
+
+### === === === === === === === === === === === === === ###
+##### Green leaf cover visual assessment in percentage #####
+### === === === === === === === === === === === === === ###
 # green visual assessment of green leaf cover
 greencov <- senescence[, c(1:6, grep("_percent$", names(senescence)))]
+names(greencov) <- gsub("_percent$", "", names(greencov))
+# convert to long format
+greencovlong  <- greencov %>%
+  pivot_longer(
+    cols = -c(tree_ID, bloc, treatment, genus, species, notes),
+    names_to = "DOY",
+    values_to = "greenCovPer",
+  ) %>%
+  unite("ID_DOY", tree_ID, DOY, sep = "_") %>%  
+  select(ID_DOY, greenCovPer)                  
 
-# get chlorophyll measurements
+# add notes back in greencov
+greencovlongwnotes <- merge(greencovlong, noteslong, by = "ID_DOY", all.x = TRUE)
+# re-add cols
+greencovlongwnotes$ID <- sub("(_\\d+)$", "", greencovlongwnotes$ID_DOY)  
+greencovlongwnotes$DOY <- sub(".*_(\\d+)$", "\\1", greencovlongwnotes$ID_DOY) 
+greencovlongwnotes$Species <- sub("^([A-Za-z]+)_.*", "\\1", greencovlongwnotes$ID_DOY)
+greencovlongwnotes$Treatment <- sub("^[^_]+_([^_]+)_B\\d+.*", "\\1", greencovlongwnotes$ID_DOY)
+greencovlongwnotes$Replicate <- sub(".*_R(\\d+)_.*", "\\1", greencovlongwnotes$ID_DOY)
+# reorganize columns 
+greencovlongwnotes <- greencovlongwnotes[,c("ID","Species", "Treatment", "Replicate", "Note", "DOY", "greenCovPer")]
+# change to numeric
+greencovlongwnotes$DOY <- as.numeric(greencovlongwnotes$DOY)
+greencovlongwnotes$greenCovPer <- as.numeric(greencovlongwnotes$greenCovPer)
+### === === === === === === === ###
+##### Chlorophyll measurements #####
+### === === === === === === === ###
 chl <- senescence[, c(names(senescence)[1:6], grep("^[0-9]+_[1-3]$", names(senescence), value = TRUE))]
+# convert chl measurements to long df. What this does is that it grabs each colname, gets the 3 digits that are the DOY, get the number after sep _ which is the measurement number and converts the df to long format
+chlorolong <- chl %>%
+  pivot_longer(
+    cols = matches("^\\d{3}_\\d+$"),  # select only columns like 248_1, 255_2, etc.
+    names_to = c("DOY", "measurement"),
+    names_sep = "_",
+    values_to = "chlValue"
+  ) %>%
+  mutate(
+    ID_DOY = paste(tree_ID, DOY, sep = "_")
+  ) %>%
+  select(ID_DOY, measurement, chlValue)
+# add notes back in chl 
+chlorolongwnotes <- merge(chlorolong, noteslong, by = "ID_DOY", all.x = TRUE)
 
+# re-add cols
+chlorolongwnotes$ID <- sub("(_\\d+)$", "", chlorolongwnotes$ID_DOY)  
+chlorolongwnotes$DOY <- sub(".*_(\\d+)$", "\\1", chlorolongwnotes$ID_DOY) 
+chlorolongwnotes$Species <- sub("^([A-Za-z]+)_.*", "\\1", chlorolongwnotes$ID_DOY)
+chlorolongwnotes$Treatment <- sub("^[^_]+_([^_]+)_B\\d+.*", "\\1", chlorolongwnotes$ID_DOY)
+chlorolongwnotes$Replicate <- sub(".*_R(\\d+)_.*", "\\1", chlorolongwnotes$ID_DOY)
+# reorganize columns 
+chlorolongwnotes <- chlorolongwnotes[,c("ID","Species", "Treatment", "Replicate", "Note", "DOY", "measurement", "chlValue")]
+# change to numeric
+chlorolongwnotes$DOY <- as.numeric(chlorolongwnotes$DOY)
+chlorolongwnotes$chlValue <- as.numeric(chlorolongwnotes$chlValue)
+# rename df for something better
+leafdrop <- leafdrop
+greenleafcover <- greencovlongwnotes
+chl <- chlorolongwnotes
+str(chl)
