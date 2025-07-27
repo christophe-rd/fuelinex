ecbbc6896d66c3162518e150f28358832ef594ca
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-07-27 16:45:23 -0700

    cheatsheet

--- analyses/rcode/cheatSheet.R
+++ analyses/rcode/cheatSheet.R
@@ -1,5 +1,8 @@
 ## R cheat sheet with useful stuff in it.
 
+# when converting to numeric 
+# if I have 
+# always use as.numeric(as.character()). It's a safety measure, otherwise if I convert to numeric as.numeric something bad will happen
 ### Look up differences between two data frames
 setdiff(df1$replicates, df2$replicates)
 
@@ -10,4 +13,4 @@ phenos<-phenos%>%tidyr::spread(phase, doy)
 nbobsperID <- d %>% count(plantNickname)
 
 ### Extracting Characters from a specific position
-cg18$species<-substr(cg18$Ind, 0,6) # here $species will return the characters from the position 0 to 6
+cg18$species<-substr(cg18$Ind, 0,6) # here $species will return the characters from the position 0 to 6a
