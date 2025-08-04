cae4a86e30a3f5bdda3592ce1f111e67bc09c3fe
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-08-04 11:54:08 -0700

    cleaning measurements update

--- analyses/cleaning/source/cleaningMeasurements.R
+++ analyses/cleaning/source/cleaningMeasurements.R
@@ -27,7 +27,7 @@ d<-read.csv("input/TreeMeasurements.csv", header=TRUE)
 d <- subset(d, !is.na(bloc))
 
 # start small with 24 only
-w2024 <- d[, c(1:11)]
+w2024 <- d[, c(1:11, 17:18)]
 
 # add year column
 w2024$year <- 2024
@@ -35,22 +35,44 @@ w2024$year <- 2024
 w2024$month <- "february"
 
 # standardize colnames
-colnames(w2024) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "year", "month")
+colnames(w2024) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "valid_height", "valid_diameter", "year", "month")
 
-# 2025
-w2025 <- d[, c(1:5, 12:16, 19)]
+# Winter 2025
+w2025 <- d[, c(1:5, 12:16, 19, 17:18)]
 
 # add year column
 w2025$year <- 2025
 w2025$month <- ifelse(w2025$doy > 31 & w2025$doy < 60, "february", "march")
 
 # standardize colnames
-colnames(w2025) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "year", "month")
+colnames(w2025) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "valid_height", "valid_diameter", "year", "month")
+
+# Fall of 2025 
+#### To be continued
+
 
 # bind both dfs 
-binded <- rbind(w2024, w2025)
+binded2 <- rbind(w2024, w2025)
 
-ggplot(binded) +
-  geom_point(aes(x = year, y = diameter, color = species)) +
-  facet_wrap(species~treatment, ncol = 6, nrow = 7) +
-  ylim(0, 20)
+# remove all the dead trees
+binded <- subset(binded2, notes != "dead")
+
+#### Go through the notes ####
+# make changes if necessary
+notesdf <- subset(binded, notes != "")
+
+binded$valid_height[which(binded$notes == "measured shoot is dead, another shoot is measured")] <- "no"
+
+# get invalidy for 2025 and remove measurements for 2024
+### height
+binded$Height <- binded$height
+idheighttochange <- binded$tree_ID[which(binded$year == "2025" & binded$valid_height == "no")]
+binded$Height[which(binded$tree_ID %in% idheighttochange & binded$year == "2024")] <- NA
+#### diameter
+binded$Diameter <- binded$diameter
+iddiamtochange <- binded$tree_ID[which(binded$year == "2025" & binded$valid_diameter == "no")]
+binded$Diameter[which(binded$tree_ID %in% iddiamtochange & binded$year == "2024")] <- NA
+
+# write up csv
+write.csv2(binded, "output/cleanedMeasurements.csv")
+
