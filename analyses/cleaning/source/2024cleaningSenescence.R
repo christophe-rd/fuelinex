a8fdd0f775762051f321e01a2032dc1072783432
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-08-11 15:59:41 -0700

    finished cleaning leaf senescence 2024!

--- analyses/cleaning/source/2024cleaningSenescence.R
+++ analyses/cleaning/source/2024cleaningSenescence.R
@@ -7,6 +7,8 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 
+options(max.print = 150) 
+
 # Set the path 
 setwd("/Users/christophe_rouleau-desrochers/github/fuelinex/analyses/")
 
@@ -24,17 +26,34 @@ senescence$Notes[which(senescence$notes == "doy262: no green leaves; doy 283: la
 senescence$Notes[which(senescence$notes == "doy248:1 lateral branch dead; doy318: last leaves fell")] <- "doy248: one lateral branch dead; doy318: last leaves fell" 
 senescence$Notes[which(senescence$notes == "doy248:barely dead measurement on only one leaf that fell after measure; doy255: no leaves")] <- "doy248: barely dead measurement on only one leaf that fell after measure; doy255: no leaves" 
 senescence$Notes[which(senescence$notes == "doy155:POPLAR IN THE POT . I cut the stem. Roots still inside; doy290: last leaves fell")] <- "doy155: POPLAR IN THE POT . I cut the stem. Roots still inside; doy290: last leaves fell" 
-senescence$Notes[which(senescence$notes == "doy276: no green leaves; doyna:last leaves fell")] <- "doy276: no green leaves; doyNA: last leaves fell" 
-senescence$Notes[which(senescence$notes == "doy262:seam sick; doyna: last leaves fell")] <- "doy262: seems sick; doyNA: last leaves fell" 
-ncol(senescence)
+senescence$Notes[which(senescence$notes == "doy276: no green leaves; doyna:last leaves fell")] <- "doy276: no green leaves" 
+senescence$Notes[which(senescence$notes == "doy262:seam sick; doyna: last leaves fell")] <- "doy262: seems sick"
+senescence$Notes[which(senescence$tree_ID == "Poba_WarmS/CoolF_B3_R15")] <- "doy000: three branches died because of herbivory"
+senescence$Notes[which(senescence$notes == "dead")] <- "doy000: dead"
+
+# clean data where it's necessary
+senescence$`248_1`[grepl("doy248 measurement percent wrong", senescence$notes)]<- NA
+senescence$`248_2`[grepl("doy248 measurement percent wrong", senescence$notes)]<- NA
+senescence$`248_3`[grepl("doy248 measurement percent wrong", senescence$notes)]<- NA
+
+senescence$`248_1`[which(grepl("doy255: previous measurement percent wrong", senescence$notes))] <- NA
+senescence$`248_2`[which(grepl("doy255: previous measurement percent wrong", senescence$notes))] <- NA
+senescence$`248_3`[which(grepl("doy255: previous measurement percent wrong", senescence$notes))] <- NA
+
+senescence$`255_1`[grepl("doy255 wrong measure percent", senescence$notes)]<- NA
+senescence$`255_2`[grepl("doy255 wrong measure percent", senescence$notes)]<- NA
+senescence$`255_3`[grepl("doy255 wrong measure percent", senescence$notes)]<- NA
+
+senescence$notes[which(grepl("previous measurement percent wrong", senescence$notes))]
+
 sub <- senescence[, c(1:5, 63)]
 
-extract_notes <- function(notes) {
-  if (notes == "") {
+extract_notes <- function(Notes) {
+  if (Notes == "") {
     return(NULL)
   }
   # Split the notes by semicolon to handle multiple DOY entries
-  note_parts <- strsplit(notes, ";")[[1]]
+  note_parts <- strsplit(Notes, ";")[[1]]
   # Extract DOY and note for each part
   doy_notes <- lapply(note_parts, function(part) {
     doy <- sub(".*doy(\\d+):.*", "\\1", part)
@@ -53,6 +72,9 @@ notes_list <- lapply(notes_list, function(x) if (is.null(x)) data.frame(DOY = NA
 notes_df <- do.call(rbind, notes_list)
 notes_df$tree_ID <- rep(sub$tree_ID, sapply(notes_list, function(x) nrow(x)))
 
+# change 000 to NA
+notes_df$DOY[which(notes_df$DOY == "000")] <- NA
+
 ### === === === === === === === === === === === === ###
 #### Get a column for leaf drop and greeness loss ####
 ### === === === === === === === === === === === === ###
@@ -66,14 +88,16 @@ subby$Note[which(subby$Note == "cant measure chl")] <- "greenessLoss"
 subby$Note[which(subby$Note == "last leaves fell")] <- "leafDrop" 
 subby$Note[which(subby$Note == "no leaves")] <- "leafDrop" 
 
-leafdrop <- subby %>%
-  group_by(tree_ID, Note) %>%
-  summarise(DOY = first(DOY), .groups = "drop") %>%
-  pivot_wider(
-    id_cols = tree_ID,
-    names_from = Note,
-    values_from = DOY
-  )
+# reshape to get 3 cols
+agg <- aggregate(DOY ~ tree_ID + Note, data = subby, FUN = function(x) x[1])
+leafdrop <- reshape(
+  agg,
+  idvar = "tree_ID",
+  timevar = "Note",
+  direction = "wide"
+)
+names(leafdrop) <- sub("^DOY\\.", "", names(leafdrop))
+leafdrop24 <- leafdrop
 
 ### === === === === === === === === === ###
 #### Organize chl and green leaf cover ####
@@ -84,7 +108,6 @@ leafdrop <- subby %>%
 ### === === === === === ###
 # get notes df and remove last leaves fell and no green leaves becasue they've been adressed separately
 notes_df_upd <- subset(notes_df, !(Note %in% c("no green leaves", "last leaves fell")))
-
 # remove rows that don't have notes
 noteslong <- notes_df_upd[!is.na(notes_df_upd$DOY) & notes_df_upd$DOY != "", ]
 # paste ID and DOY
@@ -96,62 +119,77 @@ noteslong <- noteslong[, c("ID_DOY", "Note")]
 ##### Green leaf cover visual assessment in percentage #####
 ### === === === === === === === === === === === === === ###
 # green visual assessment of green leaf cover
-greencov <- senescence[, c(1:6, grep("_percent$", names(senescence)))]
+greencov <- senescence[, c(1:5, grep("_percent$", names(senescence)))]
 names(greencov) <- gsub("_percent$", "", names(greencov))
-# convert to long format
-greencovlong  <- greencov %>%
-  pivot_longer(
-    cols = -c(tree_ID, bloc, treatment, genus, species, notes),
-    names_to = "DOY",
-    values_to = "greenCovPer",
-  ) %>%
-  unite("ID_DOY", tree_ID, DOY, sep = "_") %>%  
-  select(ID_DOY, greenCovPer)                  
+          
+# pivot (and remove old notes col)
+vec <- c("tree_ID", "bloc", "treatment", "genus", "species", "Notes")
+pivot_cols <- setdiff(names(greencov), vec)
+greencovlong <- reshape(
+  data = greencov,
+  varying = pivot_cols,
+  v.names = "greenCovPer",
+  timevar = "DOY",
+  times = pivot_cols,
+  direction = "long"
+)
+
+rownames(greencovlong) <- NULL
+
+# ADDITIONAL CLEANING 
+# assuming it's a mistake that should have been entered in chl
+greencovlong$greenCovPer[which(greencovlong$greenCovPer == "19.2")] <- NA
+# assuming it's a typo that should've been 70
+greencovlong$greenCovPer[which(greencovlong$greenCovPer == "79")] <- 70
+greencovlong$greenCovPer[which(greencovlong$greenCovPer == "")] <- NA
+greencovlong$greenCovPer[which(greencovlong$greenCovPer == "na")] <- NA
+
+greencovlong$ID_DOY <- paste(greencovlong$tree_ID, greencovlong$DOY, sep="_")
 
 # add notes back in greencov
 greencovlongwnotes <- merge(greencovlong, noteslong, by = "ID_DOY", all.x = TRUE)
-# re-add cols
-greencovlongwnotes$ID <- sub("(_\\d+)$", "", greencovlongwnotes$ID_DOY)  
-greencovlongwnotes$DOY <- sub(".*_(\\d+)$", "\\1", greencovlongwnotes$ID_DOY) 
-greencovlongwnotes$Species <- sub("^([A-Za-z]+)_.*", "\\1", greencovlongwnotes$ID_DOY)
-greencovlongwnotes$Treatment <- sub("^[^_]+_([^_]+)_B\\d+.*", "\\1", greencovlongwnotes$ID_DOY)
-greencovlongwnotes$Replicate <- sub(".*_R(\\d+)_.*", "\\1", greencovlongwnotes$ID_DOY)
-# reorganize columns 
-greencovlongwnotes <- greencovlongwnotes[,c("ID","Species", "Treatment", "Replicate", "Note", "DOY", "greenCovPer")]
+
 # change to numeric
 greencovlongwnotes$DOY <- as.numeric(greencovlongwnotes$DOY)
 greencovlongwnotes$greenCovPer <- as.numeric(greencovlongwnotes$greenCovPer)
+
+greenleafcover24 <- greencovlongwnotes[,c("tree_ID","bloc", "treatment", "genus", "species", "DOY", "greenCovPer", "Note")]
+
 ### === === === === === === === ###
 ##### Chlorophyll measurements #####
 ### === === === === === === === ###
-chl <- senescence[, c(names(senescence)[1:6], grep("^[0-9]+_[1-3]$", names(senescence), value = TRUE))]
-# convert chl measurements to long df. What this does is that it grabs each colname, gets the 3 digits that are the DOY, get the number after sep _ which is the measurement number and converts the df to long format
-chlorolong <- chl %>%
-  pivot_longer(
-    cols = matches("^\\d{3}_\\d+$"),  # select only columns like 248_1, 255_2, etc.
-    names_to = c("DOY", "measurement"),
-    names_sep = "_",
-    values_to = "chlValue"
-  ) %>%
-  mutate(
-    ID_DOY = paste(tree_ID, DOY, sep = "_")
-  ) %>%
-  select(ID_DOY, measurement, chlValue)
+chl <- senescence[, c(names(senescence)[1:5], grep("^[0-9]+_[1-3]$", names(senescence), value = TRUE))]
+
+# now pivot!
+pivot_cols <- grep("^\\d{3}_\\d+$", names(chl), value = TRUE)
+
+chlorolong <- reshape(
+  chl,
+  varying = pivot_cols,
+  v.names = "chlValue",
+  timevar = "DOY_measurement",
+  times = pivot_cols,
+  idvar = setdiff(names(chl), pivot_cols),
+  direction = "long"
+)
+
+tmp <- do.call(rbind, strsplit(as.character(chl_long$DOY_measurement), "_"))
+chlorolong$DOY <- tmp[, 1]
+chlorolong$measurement <- tmp[, 2]
+rownames(chlorolong) <- NULL
+
+chlorolong$ID_DOY <- paste(chlorolong$tree_ID, chlorolong$DOY, sep="_")
+
 # add notes back in chl 
 chlorolongwnotes <- merge(chlorolong, noteslong, by = "ID_DOY", all.x = TRUE)
 
-# re-add cols
-chlorolongwnotes$ID <- sub("(_\\d+)$", "", chlorolongwnotes$ID_DOY)  
-chlorolongwnotes$DOY <- sub(".*_(\\d+)$", "\\1", chlorolongwnotes$ID_DOY) 
-chlorolongwnotes$Species <- sub("^([A-Za-z]+)_.*", "\\1", chlorolongwnotes$ID_DOY)
-chlorolongwnotes$Treatment <- sub("^[^_]+_([^_]+)_B\\d+.*", "\\1", chlorolongwnotes$ID_DOY)
-chlorolongwnotes$Replicate <- sub(".*_R(\\d+)_.*", "\\1", chlorolongwnotes$ID_DOY)
-# reorganize columns 
-chlorolongwnotes <- chlorolongwnotes[,c("ID","Species", "Treatment", "Replicate", "Note", "DOY", "measurement", "chlValue")]
+# ADDITIONAL CLEANING
+unique(chlorolongwnotes$chlValue)
+chlorolongwnotes$chlValue[which(chlorolongwnotes$chlValue == "")] <- NA
+
 # change to numeric
 chlorolongwnotes$DOY <- as.numeric(chlorolongwnotes$DOY)
 chlorolongwnotes$chlValue <- as.numeric(chlorolongwnotes$chlValue)
-# rename df for something better
-leafdrop24 <- leafdrop
-greenleafcover24 <- greencovlongwnotes
-chl24 <- chlorolongwnotes
+
+# reorganize columns 
+chl24 <- chlorolongwnotes[,c("tree_ID","bloc", "treatment", "genus", "species", "DOY", "measurement", "chlValue", "Note")]
