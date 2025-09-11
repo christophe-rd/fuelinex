47b0cb08b1158194a97f5aba4ed118a094dc41b5
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-09-11 10:39:53 -0700

    came back to simulate data for fuelinex

--- analyses/modeling/simDataFuelinex.R
+++ analyses/modeling/simDataFuelinex.R
@@ -25,290 +25,172 @@ setwd("/Users/christophe_rouleau-desrochers/github/fuelinex/analyses")
 #### Step 2. Simulate data ####
 # === === === === === === === === === === === === === === === === 
 
-
-
 ### do this: reconcile script grouping errors because my model doesn't converge ds
-
-
 
 # === === === === === === === #
 ##### Biomass X Gdd cons #####
 # === === === === === === === #
 a <- 5
 b <- 0.9
-rep <- 50
-sigma_y <- 0.6
+sigma_y <- 0.4
 sigma_treat <- 0.2
-error <- rnorm(rep, 0, sigma_y)
+sigma_spp <- 0.3
 
-# === === === === === === === === === #
-### treatment ww ###
-# === === === === === === === === === #
-# set spp names and ids 
-ids_ww <- paste0(rep("ww", each = rep), "", 1:rep)  
-n_ids_ww <- length(ids_ww)
+n_treat <- 20
+n_spp <- 25
+n_per_treat <- 20
 
-# partial pooling
-a_treat_ww <- rnorm(rep, 0, sigma_treat)
+N <-n_treat*n_per_treat*n_spp
+
+error <- rnorm(N, 0, sigma_y)
+
+ids <- rep(rep(1:n_per_treat, times = n_treat), times = n_spp)
+treat <- rep(rep(1:n_treat, each = n_per_treat), times = n_spp)
+spp <- rep(rep(1:n_spp, each = n_per_treat), each = n_treat)
+
+# partial pooling 
+### here, I am not partial pooling on ids, because unlike wildchrokie, my ids here are replicates, so they are the repeated measurements
+a_treat <- rnorm(n_treat, 0, sigma_treat)
+a_spp <- rnorm(n_spp, 0, sigma_spp)
 
 # gdd per treatment and devide by constant
-gdd_ww <- round(rnorm(rep, 2000, 100))
-gddcons_ww <- gdd_ww / 200
-
-# calculate biomass
-biomass_ww <- a + a_treat_ww + b * gddcons_ww + error
+gdd <- rnorm(N, 2000, 100)/ 200
 
 # set df
-sim_biomass_ww <- data.frame(
-  ids = ids_ww,
-  treat = "ww",
-  gddcons = gddcons_ww,
-  b = b,
+sim_biomass <- data.frame(
+  ids = ids,
+  treat = treat,
+  spp = spp,
   a = a,
-  a_ids = a_treat_ww,
-  sigma_treat = sigma_treat,
-  biomass = biomass_ww
+  b = b,
+  gdd = gdd,
+  error = error
 )
-sim_biomass_ww
-plot(biomass~gddcons, sim_biomass_ww)
+sim_biomass
+
+sim_biomass$a_treat <-  a_treat[sim_biomass$treat]
+sim_biomass$a_spp <-  a_spp[sim_biomass$spp]
+
+sim_biomass$ids_uni <- paste(sim_biomass$ids, sim_biomass$treat, sep = "_")
+
+# calculate biomass
+sim_biomass$biomass <- c(
+  sim_biomass$a + 
+    sim_biomass$a_treat + 
+    sim_biomass$a_spp +
+    sim_biomass$b * sim_biomass$gdd + 
+    sim_biomass$error
+)
+sim_biomass
 
 
-# First, run model with ww only
-fitbiomass_ww <- stan_glm(
-  biomass ~ gddcons,  
-  data = sim_biomass_ww,
-  chains = 4,
-  iter = 4000,
-  core=4
-) # ok, so that works
-
-# Second, run model with ww only, but partial pool for ids
-fitbiomass_ww <- stan_lmer(
-  biomass ~ gddcons + (1 | ids),  
-  data = sim_biomass_ww,
+# run model
+fitbiomass <- stan_lmer(
+  biomass ~ gdd + (1 | treat) + (1 | spp),  
+  data = sim_biomass,
   chains = 4,
   iter = 4000,
   core=4
 ) 
+fitbiomass
 
-# === === === === === === === === === #
-### treatment cc ###
-# === === === === === === === === === #
-# set spp names and ids 
-ids_cc <- paste0(rep("cc", each = rep), "", 1:rep)  
-n_ids_cc <- length(ids_cc)
 
-# partial pooling
-a_treat_cc <- rnorm(rep, 0, sigma_treat)
+# === === === === === === #
+# PARAMETER RECOVERY ####
+# === === === === === === #
+df_fit <- as.data.frame(fitbiomass)
 
-# gdd per treatment and devide by constant
-gdd_cc <- round(rnorm(rep, 1600, 100))
-gddcons_cc <- gdd_cc / 200
-
-# calculate biomass
-biomass_cc <- a + a_treat_cc + b * gddcons_cc + error
-
-# set df
-sim_biomass_cc <- data.frame(
-  ids = ids_cc,
-  treat = "cc",
-  gddcons = gddcons_cc,
-  b = b,
-  a = a,
-  a_ids = a_treat_cc,
-  sigma_treat = sigma_treat,
-  biomass = biomass_cc
+# recover slope
+colnames(df_fit)
+# grab treat nested in spp
+treat_cols <- colnames(df_fit)[grepl(" treat:", colnames(df_fit))]
+treat_cols <- treat_cols[1:length(treat_cols)]
+treat_df <- df_fit[, colnames(df_fit) %in% treat_cols]
+# change their names
+colnames(treat_df) <- sub(".*treat:([^]]+)\\]$", "\\1", colnames(treat_df))
+# empty treat dataframe
+treat_df2 <- data.frame(
+  treat = character(ncol(treat_df)),
+  fit_a_treat = numeric(ncol(treat_df)),  
+  fit_a_treat_per5 = NA, 
+  fit_a_treat_per95 = NA
 )
-sim_biomass_cc
+treat_df2
 
-# === === === === === === === === === #
-# bind both biomass df
-sim_biomass <- rbind(sim_biomass_ww, sim_biomass_cc)
-# === === === === === === === === === #
-
-simplot <- ggplot(sim_biomass, aes(x = gddcons, y = biomass, color = treat)) +
-  geom_point() +
-  scale_color_manual(values = c("ww" = "#FF8C00", "cc" = "#1f78b4")) +
-  # geom_smooth(method = "lm", se = FALSE) +
-  theme_minimal()
-simplot
-# save ggplot
-ggsave("figures/sim/simplot_biomass.jpeg", simplot, width = 6, height = 4)
-
-
-# look up difference by treat
-treatcomparison_biomass <- ggplot(sim_biomass, aes(x = treat, y = biomass, color = treat)) +
-  geom_boxplot(
-    width = 0.3, 
-    alpha = 0.2, 
-    outlier.shape = NA,
-    color = "black"   
-  ) +
-  geom_jitter(
-    width = 0.1, 
-    alpha = 0.6, 
-    size = 2
-  ) +
-  scale_color_manual(values = c("ww" = "#FF8C00", "cc" = "#1f78b4")) +
-  theme_minimal() +
-  theme(
-    legend.position = "none",  
-    plot.title = element_text(hjust = 0.5)
-  )
-treatcomparison_biomass
-ggsave("figures/sim/treatcomparison_biomass.jpeg", treatcomparison_biomass, width = 6, height = 4)
-
-####### Model #######
-if(runmodels) {
-  fitbiomass <- stan_lmer(
-    biomass ~ gddcons + (1 | treat),  
-    data = sim_biomass,
-    chains = 4,
-    iter = 4000,
-    core=4
-  )
-  fitbiomass
-  }
-
-# round to 2 digits for all columns
-sim_biomass2 <- as.data.frame(lapply(sim_biomass, function(x) if(is.numeric(x)) round(x, 3) else x))
-
-
-# parameter recovery
-fitef_biomass <- ranef(fitbiomass_ww)
-ids_df <- fitef_biomass$ids
-
-# Now extract the tree IDs and intercepts
-biomass_a_idswithranef <- data.frame(
-  ids = rownames(ids_df),
-  fit_biomass_a_ids = ids_df[["(Intercept)"]]
-)
-inter_biomass <- as.data.frame(posterior_interval(fitbiomass_ww))
-inter_biomass$messyids <- rownames(inter_biomass)
-biomass_a_ids_messy <- subset(inter_biomass, grepl("ids", messyids))
-biomass_a_ids_messy$ids <- sub(".*ids:([^]]+)]", "\\1", biomass_a_ids_messy$messyids)
-# remove non necessary columns
-biomass_a_ids_messy <- biomass_a_ids_messy[, c("ids", "5%", "95%")]
-# renames 5% and 95%
-colnames(biomass_a_ids_messy) <- c("ids", "per5", "per95")
-# merge both df by ids
-a_ids_mergedwithranef <- merge(biomass_a_idswithranef, biomass_a_ids_messy, by = "ids")
-# add simulation data and merge!
-simcoeftoplot2 <- sim_biomass_ww[, c("ids", "a_ids")]
-colnames(simcoeftoplot2) <- c("ids", "sim_biomass_a_ids")
-a_ids_mergedwithranef <- merge(simcoeftoplot2, a_ids_mergedwithranef, by = "ids")
-
-plot_a_ids_mergedwithranef<- ggplot(a_ids_mergedwithranef, aes(x = sim_biomass_a_ids, y = fit_biomass_a_ids)) +
-  geom_point(color = "blue", size = 2) +
-  geom_errorbar(aes(ymin = per5, ymax = per95), width = 0, color = "darkgray", alpha=0.1) +
-  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "red", linewidth = 1) +
-  # labs(x = "Simulated a_ids", y = "Model a_ids", title = "") +
-  theme_minimal()
-plot_a_ids_mergedwithranef
-ggsave("figures/sim/biomass_a_ids_mergedwithranef.jpeg", plot_a_ids_mergedwithranef, width = 8, height = 6)
-
-
-# === ===  === ===  === ===  === === #
-##### CC, WW on leaf drop date #####
-# === ===  === ===  === ===  === === #
-# b is + because leaf drop is later when more gdd (estimated here from leaf out to budset)
-a <- 230
-b <- 5 
-rep <- 100
-sigma_y <- 5
-sigma_treat <- 2
-error <- rnorm(rep, 0, sigma_y)
-
-# === === === === === === === === === #
-### treatment ww ###
-# === === === === === === === === === #
-# set spp names and ids 
-ids_ww <- paste0(rep("ww", each = rep), "", 1:rep)  
-n_ids_ww <- length(ids_ww)
-
-# partial pooling
-a_ids_ww <- rnorm(n_ids_ww, 0, sigma_treat)
-
-# gdd per treatment and devide by constant
-gdd_ww <- round(rnorm(rep, 2000, 100))
-gddcons_ww <- gdd_ww / 200
-
-# calculate biomass
-ldd_ww <- a + a_ids_ww + b * gddcons_ww + error
-
-# set df
-sim_ldd_ww <- data.frame(
-  ids = ids_ww,
-  treat = "ww",
-  gddcons = gddcons_ww,
-  b = b,
-  a = a,
-  a_ids = a_ids_ww,
-  sigma_y = sigma_y,
-  ldd = ldd_ww
-)
-sim_ldd_ww
-# === === === === === === === === === #
-### treatment cc ###
-# === === === === === === === === === #
-# set spp names and ids 
-ids_cc <- paste0(rep("cc", each = rep), "", 1:rep)  
-n_ids_cc <- length(ids_cc)
-
-# partial pooling
-a_ids_cc <- rnorm(n_ids_cc, 0, sigma_treat)
-
-# gdd per treatment and devide by constant
-gdd_cc <- round(rnorm(rep, 2000, 100))
-gddcons_cc <- gdd_cc / 200
-
-# calculate biomass
-ldd_cc <- a + a_ids_cc + b * gddcons_cc + error
-
-# set df
-sim_ldd_cc <- data.frame(
-  ids = ids_cc,
-  treat = "cc",
-  gddcons = gddcons_cc,
-  b = b,
-  a = a,
-  a_ids = a_ids_cc,
-  sigma_y = sigma_y,
-  ldd = ldd_cc
-)
-sim_ldd_cc
-
-
-# look up difference by treat
-treatcomparison_ldd <- ggplot(simdf, aes(x = treat, y = ldd, color = treat)) +
-  geom_boxplot(
-    width = 0.3, 
-    alpha = 0.2, 
-    outlier.shape = NA,
-    color = "black"   
-  ) +
-  geom_jitter(
-    width = 0.1, 
-    alpha = 0.6, 
-    size = 2
-  ) +
-  scale_color_manual(values = c("cc" = "#1f78b4", "ww" = "#FF8C00")) +
-  theme_minimal() +
-  theme(
-    legend.position = "none",  
-    plot.title = element_text(hjust = 0.5)
-  )
-treatcomparison_ldd
-ggsave("figures/sim/treatcomparison_ldd.jpeg", treatcomparison_ldd, width = 6, height = 4)
-
-# run model!
-if(runmodels) {
-  fitldd <- stan_lmer(
-    ldd ~ gddcons + (1 | ids),  
-    data = simdf,
-    chains = 4,
-    iter = 4000,
-    core=4
-  )
+for (i in 1:ncol(treat_df)) { # i = 1
+  treat_df2$treat[i] <- colnames(treat_df)[i]         
+  treat_df2$fit_a_treat[i] <- round(mean(treat_df[[i]]),3)  
+  treat_df2$fit_a_treat_per5[i] <- round(quantile(treat_df[[i]], probs = 0.055), 3)
+  treat_df2$fit_a_treat_per95[i] <- round(quantile(treat_df[[i]], probs = 0.945), 3)
 }
+treat_df2
 
+df_fit <- as.data.frame(fitbiomass)
+
+
+# recover spp
+# grab treat nested in spp
+spp_cols <- colnames(df_fit)[grepl(" spp:", colnames(df_fit))]
+spp_cols <- spp_cols[1:length(spp_cols)]
+spp_df <- df_fit[, colnames(df_fit) %in% spp_cols]
+# change their names
+colnames(spp_df) <- sub(".*spp:([^]]+)\\]$", "\\1", colnames(spp_df))
+# empty spp dataframe
+spp_df2 <- data.frame(
+  spp = character(ncol(spp_df)),
+  fit_a_spp = numeric(ncol(spp_df)),  
+  fit_a_spp_per5 = NA, 
+  fit_a_spp_per95 = NA
+)
+spp_df2
+
+for (i in 1:ncol(spp_df)) { # i = 1
+  spp_df2$spp[i] <- colnames(spp_df)[i]         
+  spp_df2$fit_a_spp[i] <- round(mean(spp_df[[i]]),3)  
+  spp_df2$fit_a_spp_per5[i] <- round(quantile(spp_df[[i]], probs = 0.055), 3)
+  spp_df2$fit_a_spp_per95[i] <- round(quantile(spp_df[[i]], probs = 0.945), 3)
+}
+spp_df2
+
+
+# PLOT PARAMETER RECOVERY #####
+# merge sim and fit
+treattoplot <- merge(
+  sim_biomass[!duplicated(sim_biomass$treat), 
+              c("treat", "a_treat")], 
+  treat_df2[, 
+          c("treat", "fit_a_treat", "fit_a_treat_per5", "fit_a_treat_per95")], 
+  by = "treat"
+)
+treattoplot
+
+a_treat_simXfit_plot <- ggplot(treattoplot, aes(x = a_treat, y = fit_a_treat)) +
+  geom_point(color = "#046C9A", size = 2) +
+  geom_errorbar(aes(ymin = fit_a_treat_per5, ymax = fit_a_treat_per95), width = 0, color = "darkgray", alpha=0.3) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "#B40F20", linewidth = 1) +
+  labs(x = "sim a_treat", y = "fit a_treat", title = "") +
+  theme_minimal()
+a_treat_simXfit_plot
+# ggsave!
+ggsave("figures/a_treat_simXfit_plot.jpeg", a_treat_simXfit_plot, width = 6, height = 6, units = "in", dpi = 300)
+
+
+# merge sim and fit
+spptoplot <- merge(
+  sim_biomass[!duplicated(sim_biomass$spp), 
+          c("spp", "a_spp")], 
+  spp_df2[, 
+         c("spp", "fit_a_spp", "fit_a_spp_per5", "fit_a_spp_per95")], 
+  by = "spp"
+)
+spptoplot
+
+a_spp_simXfit_plot <- ggplot(spptoplot, aes(x = a_spp, y = fit_a_spp)) +
+  geom_point(color = "#046C9A", size = 2) +
+  geom_errorbar(aes(ymin = fit_a_spp_per5, ymax = fit_a_spp_per95), width = 0, color = "darkgray", alpha=0.3) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "#B40F20", linewidth = 1) +
+  labs(x = "sim a_spp", y = "fit a_spp", title = "") +
+  theme_minimal()
+a_spp_simXfit_plot
+# ggsave!
+ggsave("figures/a_spp_simXfit_plot.jpeg", a_spp_simXfit_plot, width = 6, height = 6, units = "in", dpi = 300)
