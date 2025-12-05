77e70745c42048179a20b215cbd8eef1161af4cf
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-12-05 15:28:09 -0800

    added biomass to cleaning script

--- analyses/cleaning/source/cleaningMeasurements.R
+++ analyses/cleaning/source/cleaningMeasurements.R
@@ -5,6 +5,8 @@
 # house keeping
 rm(list=ls()) 
 options(stringsAsFactors=FALSE)
+options(max.print = 150) 
+options(digits = 5)
 
 ### ####
 # do this: need to add validity somehow into the script
@@ -17,54 +19,110 @@ library(ggplot2)
 setwd("/Users/christophe_rouleau-desrochers/github/fuelinex/analyses")
 
 # read it
-d<-read.csv("input/TreeMeasurements.csv", header=TRUE)
+meas <- read.csv("input/TreeMeasurements.csv", header=TRUE)
+meas[meas == ""] <- NA
+biom <- read.csv("input/biomass.csv", header=TRUE)
+biom[biom == ""] <- NA
+biom$aboveGroundWeight[biom$aboveGroundWeight == "na"] <- NA
+biom$belowGroundWeight[biom$belowGroundWeight == "na"] <- NA
+
+biom <- biom[, 1:ncol(biom)-1]
 
 # clean the weird rows that have nothing in them
-d <- subset(d, !is.na(bloc))
+meas <- subset(meas, !is.na(bloc))
 
 # start small with 24 only
-w2024 <- d[, c(1:11, 17:18)]
+w2024 <- meas[, c(1:11, 17:18)]
 
 # add year column
 w2024$year <- 2024
 # add month col
 w2024$month <- "february"
+w2024$season <- "winter"
 
 # standardize colnames
-colnames(w2024) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "valid_height", "valid_diameter", "year", "month")
+colnames(w2024) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "valid_height", "valid_diameter", "year", "month", "season")
 
 # Winter 2025
-w2025 <- d[, c(1:5, 12:16, 19, 17:18)]
+w2025 <- meas[, c(1:5, 12:16, 19, 17:18)]
 
 # add year column
 w2025$year <- 2025
 w2025$month <- ifelse(w2025$doy > 31 & w2025$doy < 60, "february", "march")
-
+w2025$season <- "winter"
 # standardize colnames
-colnames(w2025) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "valid_height", "valid_diameter", "year", "month")
+colnames(w2025) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "valid_height", "valid_diameter", "year", "month", "season")
 
 # Fall of 2025 
-f2025 <- d[, c(1:5, 12:16, 19, 19:ncol(d))]
+f2025 <- meas[, c(1:5, 20:ncol(meas))]
 
 # add year column
 f2025$year <- 2025
-f2025$month <- ifelse(f2025$doy > 31 & f2025$doy < 60, "february", "march")
+f2025$season <- "fall"
+colnames(f2025) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "valid_height", "valid_diameter", "notes", "year", "season")
 
+# add missing doy for measurements
+f2025$doy[which(is.na(f2025$doy) & f2025$genus == "betula")] <- 317
+f2025$doy[which(is.na(f2025$doy) & f2025$genus == "populus")] <- 328
 
+f2025$month <- ifelse(f2025$doy > 274 & f2025$doy < 305, "october", "november")
+
+# quick checks
 test <- aggregate(
-  diameterFall25 ~ species,
+  diameter ~ species,
   f2025,
   FUN = function(x) sum(is.na(x)),
   na.action = na.pass
 )
 test
 test2 <- aggregate(
-  heightFall25 ~ species,
+  height ~ species,
   f2025,
   FUN = function(x) sum(is.na(x)),
   na.action = na.pass
 )
 test2
+
+heightna <- f2025$tree_ID[which(is.na(f2025$height) & 
+                      f2025$genus != "betula" & 
+                      f2025$genus != "populus" & 
+                      f2025$genus != "pinus")]
+diameterna <- f2025$tree_ID[which(is.na(f2025$diameter) & 
+                                  f2025$genus != "betula" & 
+                                  f2025$genus != "populus" &
+                                  f2025$genus != "pinus")]
+setdiff(heightna, diameterna)
+
+# Clean biomass ####
+vec_chr <- biom$tree_ID[which(is.na(biom$aboveGroundWeight) & 
+                                biom$genus != "betula" &
+                                biom$genus != "populus" &
+                                biom$genus != "pinus")]
+vec_chr
+for (i in 7:ncol(biom)) {
+  as.numeric(biom[[i]])
+}
+vec_numabov <- biom$tree_ID[which(is.na(biom$aboveGroundWeight) & 
+                                biom$genus != "betula" &
+                                biom$genus != "populus" &
+                                biom$genus != "pinus")]
+
+vec_numbelow <- biom$tree_ID[which(is.na(biom$belowGroundWeight) & 
+                                biom$genus != "betula" &
+                                biom$genus != "populus" &
+                                biom$genus != "pinus")]
+
+t <- setdiff(diameterna, vec_numabov)
+setdiff(diameterna, vec_numbelow)
+tsub <- subset(biom, tree_ID %in% t)
+
+biomna_above <- biom$tree_ID[which(is.na(biom$aboveGroundWeight) & 
+                                  biom$genus != "betula" & 
+                                  biom$genus != "populus")]
+biomna_below <- biom$tree_ID[which(is.na(biom$belowGroundWeight) & 
+                               biom$genus != "betula" & 
+                               biom$genus != "populus")]
+setdiff(biomna_above, biomna_below)
 
 # quick checks for measured species 
 pist <- subset(f2025, species == "pinus_strobus")
@@ -78,16 +136,12 @@ prvi$tree_ID[which(is.na(prvi$diameterFall25))]
 quma <- subset(f2025, species == "quercus_macrocarpa")
 quma$noteWinter25[which(is.na(quma$diameterFall25))]
 
-# standardize colnames
-colnames(f2025) <- c("tree_ID", "bloc", "treatment", "genus", "species", "diameter", "diameterTrunk2", "height", "heightTrunk2", "doy", "notes", "valid_height", "valid_diameter", "year", "month")
-
-
 
 # bind both dfs 
-binded2 <- rbind(w2024, w2025)
+binded <- rbind(w2024, w2025, f2025)
 
 # remove all the dead trees
-binded <- subset(binded2, notes != "dead")
+# binded <- subset(binded2, notes != "dead")
 
 #### Go through the notes ####
 # make changes if necessary
@@ -95,21 +149,52 @@ notesdf <- subset(binded, notes != "")
 
 binded$valid_height[which(binded$notes == "measured shoot is dead, another shoot is measured")] <- "no"
 
-# get invalidy for 2025 and remove measurements for 2024
+# get invalidy for winter 2025 and remove measurements for winter 2024
 ### height
 binded$Height <- binded$height
-idheighttochange <- binded$tree_ID[which(binded$year == "2025" & binded$valid_height == "no")]
-binded$Height[which(binded$tree_ID %in% idheighttochange & binded$year == "2024")] <- NA
+idheighttochange <- binded$tree_ID[which(binded$year == "2025" &
+                                           binded$month != "october" &
+                                           binded$month != "november" &
+                                           binded$valid_height == "no")]
+
+idheighttochange
+binded$Height[which(binded$tree_ID %in% idheighttochange & 
+                      binded$year == "2024")] <- NA
+
 #### diameter
 binded$Diameter <- binded$diameter
-iddiamtochange <- binded$tree_ID[which(binded$year == "2025" & binded$valid_diameter == "no")]
-binded$Diameter[which(binded$tree_ID %in% iddiamtochange & binded$year == "2024")] <- NA
 
-#
+iddiamtochange <- binded$tree_ID[which(binded$year == "2025" &
+                                           binded$month != "october" &
+                                           binded$month != "november" &
+                                           binded$valid_diameter == "no")]
+binded$Diameter[which(binded$tree_ID %in% iddiamtochange & 
+                        binded$year == "2024")] <- NA
+
+# get invalidy for fall 2025 and remove measurements for winter 2025
+### height
+binded$Height <- binded$height
+idheighttochange <- binded$tree_ID[which(binded$year == "2025" &
+                                           binded$month != "february" &
+                                           binded$month != "march" &
+                                           binded$valid_height == "no")]
+
+binded$Height[which(binded$tree_ID %in% idheighttochange & 
+                      binded$year == "2024")] <- NA
+
+# Decision: the year of the measurements will be corresponding to their previous GS.e.g. winter2024 will be 2023
+binded$Year <- binded$year
+binded$Year[which(binded$year == "2024")] <- 2023
+binded$Year[which(binded$year == "2025" & 
+                    binded$season == "winter")] <- 2024
+binded$Year[which(binded$year == "2025" & 
+                    binded$season == "fall")] <- 2025
 
 # write up csv
 ### reorganize to its nicer to play with
-bindedtowrite <- binded[, c("tree_ID", "bloc", "treatment", "genus", "species", "year", "month", "doy", "Height", "Diameter", "notes")]
+bindedtowrite <- binded[, c("tree_ID", "bloc", "treatment", "genus", "species", "Year", "Height", "Diameter", "notes")]
 
+# rename for consistency
+colnames(bindedtowrite) <- c("tree_ID", "bloc", "treatment", "genus", "species", "year", "height", "diameter", "notes")
 write.csv2(bindedtowrite, "output/cleanedMeasurements.csv", row.names = FALSE)
 
