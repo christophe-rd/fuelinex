00fe93d918d38fcf16dff576aa1b7d5c4bae6dca
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-12-14 21:54:07 -0800

    a whole new set of col palets

--- analyses/rcode/colPalets.R
+++ analyses/rcode/colPalets.R
@@ -1,3 +1,9 @@
+library("MetBrewer")
+met.brewer()
+MetBrewer::colorblind.friendly("Archambault")
+MetBrewer::display_all()
+MetBrewer::met.brewer()
+MetBrewer::MetPalettes
 # wes palettes ####
 
 $BottleRocket1
@@ -89,3 +95,208 @@ c("#fD7f6f", "#7eb0d5", "#b2e061", "#bd7ebe", "#ffb55a", "#ffee65", "#beb9db", "
 "#0000b3", "#0010d9", "#0020ff", "#0040ff", "#0060ff", "#0080ff", "#009fff", "#00bfff", "#00ffff"
 
 # Color brewer #### 
+
+# Painters
+vangogh <- "#374D8D""#93A0CB""#82A866" "#C4B743" "#A35029"
+
+> MetBrewer::MetPalettes
+
+### ### ### ### ### # COLOR BLIND FRIENDLY # ### ### ### ### ### ### 
+$Egypt
+[1] "#dd5129" "#0f7ba2" "#43b284" "#fab255"
+$Egypt$colorblind
+[1] TRUE
+
+$Kandinsky
+[1] "#3b7c70" "#ce9642" "#898e9f" "#3b3a3e"
+$Kandinsky$colorblind
+[1] TRUE
+
+##### 5 colors #####
+$Greek
+[1] "#3c0d03" "#8d1c06" "#e67424" "#ed9b49" "#f5c34d"
+$Greek$colorblind
+[1] TRUE
+
+$Isfahan2
+[1] "#d7aca1" "#ddc000" "#79ad41" "#34b6c6" "#4063a3"
+$Isfahan2$colorblind
+[1] TRUE
+
+$Java
+[1] "#663171" "#cf3a36" "#ea7428" "#e2998a" "#0c7156"
+$Java$colorblind
+[1] TRUE
+
+$Johnson
+[1] "#a00e00" "#d04e00" "#f6c200" "#0086a8" "#132b69"
+$Johnson$colorblind
+[1] TRUE
+
+##### 6 colors #####
+$Hokusai2
+[1] "#abc9c8" "#72aeb6" "#4692b0" "#2f70a1" "#134b73" "#0a3351"
+$Hokusai2$colorblind
+[1] TRUE
+
+$Hokusai3
+[1] "#d8d97a" "#95c36e" "#74c8c3" "#5a97c1" "#295384" "#0a2e57"
+$Hokusai3$colorblind
+[1] TRUE
+
+$Pillement
+[1] "#a9845b" "#697852" "#738e8e" "#44636f" "#2b4655" "#0f252f"
+$Pillement$colorblind
+[1] TRUE
+
+##### 7 colors #####
+$Archambault
+[1] "#88a0dc" "#381a61" "#7c4b73" "#ed968c" "#ab3329" "#e78429" "#f9d14a"
+$Archambault$colorblind
+[1] TRUE
+
+$Cassatt1
+[1] "#b1615c" "#d88782" "#e3aba7" "#edd7d9" "#c9c9dd" "#9d9dc7" "#8282aa" "#5a5a83"
+$Cassatt1$colorblind
+[1] TRUE
+
+$Derain
+[1] "#efc86e" "#97c684" "#6f9969" "#aab5d5" "#808fe1" "#5c66a8" "#454a74"
+$Derain$colorblind
+[1] TRUE
+
+$Ingres
+[1] "#041d2c" "#06314e" "#18527e" "#2e77ab" "#d1b252" "#a97f2f" "#7e5522" "#472c0b"
+$Ingres$colorblind
+[1] TRUE
+
+$Isfahan1
+[1] "#4e3910" "#845d29" "#ae8548" "#e3c28b" "#4fb6ca" "#178f92" "#175f5d" "#054544"
+$Isfahan1$colorblind
+[1] TRUE
+
+$VanGogh3
+[1] "#e7e5cc" "#c2d6a4" "#9cc184" "#669d62" "#3c7c3d" "#1f5b25" "#1e3d14" "#192813"
+$VanGogh3$colorblind
+[1] TRUE
+
+$Veronese
+[1] "#67322e" "#99610a" "#c38f16" "#6e948c" "#2c6b67" "#175449" "#122c43"
+$Veronese$colorblind
+[1] TRUE
+
+##### 8 colors #####
+$Cassatt2
+[1] "#2d223c" "#574571" "#90719f" "#b695bc" "#dec5da" "#c1d1aa" "#7fa074" "#466c4b" "#2c4b27" "#0e2810"
+$Cassatt2$colorblind
+[1] TRUE
+
+$Demuth
+[1] "#591c19" "#9b332b" "#b64f32" "#d39a2d" "#f7c267" "#b9b9b8" "#8b8b99" "#5d6174" "#41485f" "#262d42"
+$Demuth$colorblind
+[1] TRUE
+
+$Hiroshige
+[1] "#e76254" "#ef8a47" "#f7aa58" "#ffd06f" "#ffe6b7" "#aadce0" "#72bcd5" "#528fad" "#376795" "#1e466e"
+$Hiroshige$colorblind
+[1] TRUE
+
+$OKeeffe1
+[1] "#6b200c" "#973d21" "#da6c42" "#ee956a" "#fbc2a9" "#f6f2ee" "#bad6f9" "#7db0ea" "#447fdd" "#225bb2" "#133e7e"
+$OKeeffe1$colorblind
+[1] TRUE
+
+$OKeeffe2
+[1] "#fbe3c2" "#f2c88f" "#ecb27d" "#e69c6b" "#d37750"
+[6] "#b9563f" "#92351e"
+$OKeeffe2$colorblind
+[1] TRUE
+
+$Tam
+[1] "#ffd353" "#ffb242" "#ef8737" "#de4f33" "#bb292c" "#9f2d55" "#62205f" "#341648"
+$Tam$colorblind
+[1] TRUE
+
+$Troy
+[1] "#421401" "#6c1d0e" "#8b3a2b" "#c27668" "#7ba0b4" "#44728c" "#235070" "#0a2d46"
+$Troy$colorblind
+[1] TRUE
+
+$Morgenstern
+[1] "#98768e" "#b08ba5" "#c7a2b6" "#dfbbc8" "#ffc680" "#ffb178" "#db8872" "#a56457"
+$Morgenstern$colorblind
+[1] TRUE
+
+### ### ### ### ### # NOT COLOR BLIND FRIENDLY # ### ### ### ### ### ### 
+##### 4-5 colors #####
+$Austria
+[1] "#a40000" "#16317d" "#007e2f" "#ffcd12" "#b86092" "#721b3e" "#00b7a7"
+$Austria$colorblind
+[1] FALSE
+
+$Gauguin
+[1] "#b04948" "#811e18" "#9e4013" "#c88a2c" "#4c6216" "#1a472a"
+$Gauguin$colorblind
+[1] FALSE
+
+$Navajo
+[1] "#660d20" "#e59a52" "#edce79" "#094568" "#e1c59a"
+$Navajo$colorblind
+[1] FALSE
+
+$NewKingdom
+[1] "#e1846c" "#9eb4e0" "#e6bb9e" "#9c6849" "#735852"
+$NewKingdom$colorblind
+[1] FALSE
+
+##### 6-7 colors #####
+
+$Cross
+[1] "#c969a1" "#ce4441" "#ee8577" "#eb7926" "#ffbb44" "#859b6c" "#62929a" "#004f63" "#122451"
+$Cross$colorblind
+[1] FALSE
+
+$Degas
+[1] "#591d06" "#96410e" "#e5a335" "#556219" "#418979" "#2b614e" "#053c29"
+$Degas$colorblind
+[1] FALSE
+
+$Juarez
+[1] "#a82203" "#208cc0" "#f1af3a" "#cf5e4e" "#637b31" "#003967"
+$Juarez$colorblind
+[1] FALSE
+
+$Lakota
+[1] "#04a3bd" "#f0be3d" "#931e18" "#da7901" "#247d3f" "#20235b"
+$Lakota$colorblind
+[1] FALSE
+
+##### 8 and more colors #####
+$Manet
+[1] "#3b2319" "#80521c" "#d29c44" "#ebc174" "#ede2cc" "#7ec5f4" "#4585b7" "#225e92" "#183571" "#43429b" "#5e65be"
+$Manet$colorblind
+[1] FALSE
+
+$Renoir
+[1] "#17154f" "#2f357c" "#6c5d9e" "#9d9cd5" "#b0799a" "#f6b3b0" "#e48171" "#bf3729" "#e69b00" "#f5bb50" "#ada43b" "#355828"
+$Renoir$colorblind
+[1] FALSE
+
+$Signac
+[1] "#fbe183" "#f4c40f" "#fe9b00" "#d8443c" "#9b3441" "#de597c" "#e87b89" "#e6a2a6" "#aa7aa1" "#9f5691" "#633372" "#1f6e9c" "#2b9b81" "#92c051"
+$Signac$colorblind
+[1] FALSE
+
+$Wissing
+[1] "#4b1d0d" "#7c291e" "#ba7233" "#3a4421" "#2d5380"
+$Wissing$colorblind
+[1] FALSE
+
+
+
+
+
+
+
+
+
