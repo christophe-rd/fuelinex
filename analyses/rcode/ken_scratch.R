5d12dfc2cb0a0b74326bc215c8dff6ef2f9c5b8a
Ken Michiko Samson <shmkkmhs12@gmail.com>
2026-02-16 15:52:55 -0800

    add full model

---
+++ analyses/rcode/ken_scratch.R
@@ -0,0 +1,152 @@
+## Started 16 February 2026
+## By Ken
+
+## STAT 547 model
+
+# housekeeping
+rm(list=ls())  
+options(stringsAsFactors=FALSE)
+
+#setwd
+if(length(grep("christophe", getwd()) > 0)) { 
+  setwd("/Users/christophe_rouleau-desrochers/github/fuelinex/analyses")
+} else if(length(grep("Ken", getwd()) > 0)){
+  setwd("/Users/Ken Michiko Samson/Documents/Temporal Ecology Lab/fuelinex/analyses")
+}
+
+library(ggplot2)
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+parallel:::setDefaultClusterOptions(setup_strategy = "sequential")
+
+util <- new.env()
+source('mcmc_analysis_tools_rstan.R', local=util)
+source('mcmc_visualization_tools.R', local=util)
+
+mea <- read.csv2("output/cleanedMeasurements.csv", sep = ",", header = TRUE)
+
+mea$spp_num <- match(mea$genus, unique(mea$genus))
+mea$treeid_num <- match(mea$tree_ID, unique(mea$tree_ID))
+mea <- mea[which(!is.na(mea$height) & !is.na(mea$diameter)),]
+mea$height   <- as.numeric(mea$height)
+mea$diameter <- as.numeric(mea$diameter)
+mea <- mea[, -1]
+
+biom <- read.csv("input/biomass.csv")
+# biomass now
+biom$aboveGroundWeight <- as.numeric(biom$aboveGroundWeight)
+
+d <- reshape(mea, timevar = 'year',
+             idvar = c('tree_ID', 'bloc', 'treatment', 'genus', 'species',
+                       'spp_num', 'treeid_num'),
+             direction = 'wide')
+
+d$vol.2023 <- d$diameter.2023^2 * d$height.2023
+d$vol.2024 <- d$diameter.2024^2 * d$height.2024
+d$vol.2025 <- d$diameter.2025^2 * d$height.2025
+d$volinc1 <- d$vol.2024 - d$vol.2023
+d$volinc2 <- d$vol.2025 - d$vol.2024
+
+d <- subset(d, volinc1 > 0 & volinc2 > 0)
+
+d$s <- NA
+d$f <- NA
+trt <- unique(d$treatment)
+s <- c(0, 1, 0, 1, 0, 1)
+f <- c(0, 0, 1, 1, 0, 1)
+for(i in 1:length(trt)){
+  idx <- which(d$treatment == trt[i])
+  d$s[idx] <- s[i]
+  d$f[idx] <- f[i]
+}
+d$sf <- d$s * d$f
+
+biom$aboveGroundWeight <- as.numeric(biom$aboveGroundWeight)
+d_allo <- subset(mea, year == "2025")
+d_allo <- merge(d_allo, biom[, c("tree_ID","aboveGroundWeight")], by = "tree_ID")
+d_allo <- subset(d_allo, !is.na(diameter) & !is.na(height) & aboveGroundWeight > 0)
+
+# Fit model
+
+data <- list("N_allo" = nrow(d_allo),
+             "d_allo" = d_allo$diameter,
+             "h_allo" = d_allo$height,
+             "N_spp" = length(unique(d$species)),
+             "spp_allo" = d_allo$spp_num,
+             "agb_allo" = d_allo$aboveGroundWeight,
+             "N" = nrow(d),
+             "d0" = d$diameter.2023,
+             "h0" = d$height.2023,
+             "d1" = d$diameter.2024,
+             "h1" = d$height.2024,
+             "d2" = d$diameter.2025,
+             "h2" = d$height.2025,
+             "s" = d$s,
+             "f" = d$f,
+             "sf" = d$sf,
+             "spp" = d$spp_num)
+
+set.seed(1)
+
+inits <- function(chain_id){
+  params <- list("b1" = as.array(rlnorm(7, log(0.5), 0.3)),
+                 "b2" = as.array(rnorm(7, 0, 1)),
+                 "s_allo" = as.array(abs(rnorm(7, 0, 1))),
+                 "a1" = as.array(rnorm(7, 0, 1)),
+                 "as1" = as.array(rnorm(7, 0, 1)),
+                 "af1" = as.array(rnorm(7, 0, 1)),
+                 "asf1" = as.array(rnorm(7, 0, 1)),
+                 "a2" = as.array(rnorm(7, 0, 1)),
+                 "as2" = as.array(rnorm(7, 0, 1)),
+                 "af2" = as.array(rnorm(7, 0, 1)),
+                 "asf2" = as.array(rnorm(7, 0, 1)),
+                 "s_y" = as.array(abs(rnorm(7, 0, 1)))
+                 )
+  return(params)
+}
+
+fit <- stan("stan/fullModel.stan",
+            data = data, init = inits, seed = 1,
+            warmup = 1000, iter = 2000, refresh = 500, chains = 4)
+
+diagnostics <- util$extract_hmc_diagnostics(fit)
+print(util$check_all_hmc_diagnostics(diagnostics))
+
+samples <- util$extract_expectand_vals(fit)
+names <- c(grep('b1', names(samples), value = TRUE),
+           grep('b2', names(samples), value = TRUE),
+           grep('s_allo', names(samples), value = TRUE),
+           grep('a1', names(samples), value = TRUE),
+           grep('as1', names(samples), value = TRUE),
+           grep('af1', names(samples), value = TRUE),
+           grep('asf1', names(samples), value = TRUE),
+           grep('a2', names(samples), value = TRUE),
+           grep('as2', names(samples), value = TRUE),
+           grep('af2', names(samples), value = TRUE),
+           grep('asf2', names(samples), value = TRUE),
+           grep('s_y', names(samples), value = TRUE))
+base_samples <- util$filter_expectands(samples, names)
+print(util$check_all_expectand_diagnostics(base_samples))
+
+util$plot_expectand_pushforward(expectand_vals = samples[["b1[1]"]],
+                                B = 100,
+                                display_name = "B1",
+                                flim = c(-1, 1))
+
+names <- c("b1[1]",
+           "b2[1]",
+           "s_allo[1]",
+           "a1[1]",
+           "as1[1]",
+           "af1[1]",
+           "asf1[1]",
+           "a2[1]",
+           "as2[1]",
+           "af2[1]",
+           "asf2[1]",
+           "s_y[1]")
+
+pdf('scratch_pairs.pdf', height = 9, width = 9)
+util$plot_div_pairs(names, names, samples, diagnostics)
+dev.off()
---
+++ analyses/stan/fullModel.stan
@@ -0,0 +1,102 @@
+// Started 16 February 2026
+// By Ken
+
+// Combined allometry and carryover model
+
+data{
+  int <lower = 0> N_allo;
+  vector[N_allo] d_allo;
+  vector[N_allo] h_allo;
+  int <lower = 0> N_spp;
+  array[N_allo] int spp_allo;
+  array[N_allo] real agb_allo;
+  
+  int <lower = 0> N;
+  vector[N] d0; // initial diameter measurements
+  vector[N] h0; // initial height measurements
+  vector[N] d1;
+  vector[N] h1;
+  vector[N] d2;
+  vector[N] h2;
+  vector[N] s; // dummy variable for spring 
+  vector[N] f; // dummy variable for fall
+  vector[N] sf; // dummy variable for interation between wspring and wfall
+  array[N] int spp;
+}
+
+parameters{
+  // allometry model
+  vector<lower = 0>[N_spp] b1;
+  vector[N_spp] b2;
+  vector[N_spp] s_allo;
+  
+  // treatment effects on year 1 growth
+  vector[N_spp] a1;
+  vector[N_spp] as1;
+  vector[N_spp] af1;
+  vector[N_spp] asf1;
+  
+  // treatment effects on year 2 growth
+  vector[N_spp] a2;
+  vector[N_spp] as2;
+  vector[N_spp] af2;
+  vector[N_spp] asf2;
+  
+  vector[N_spp] s_y;
+}
+
+transformed parameters{
+  array[N_allo] real agb_allo_pred;
+  
+  for(i in 1:N_allo){
+    agb_allo_pred[i] = b1[spp_allo[i]] * (d_allo[i]^2 * h_allo[i])^b2[spp_allo[i]];
+  }
+  
+  array[N] real agb0;
+  array[N] real agb1;
+  array[N] real agb2;
+  array[N] real delta1;
+  array[N] real delta2;
+  array[N] real delta1_pred;
+  array[N] real delta2_pred;
+  
+  for(i in 1:N){
+    agb0[i] = b1[spp[i]] * (d0[i]^2 * h0[i])^b2[spp[i]];
+    agb1[i] = b1[spp[i]] * (d1[i]^2 * h1[i])^b2[spp[i]];
+    agb2[i] = b1[spp[i]] * (d2[i]^2 * h2[i])^b2[spp[i]];
+    
+    delta1[i] = agb1[i] - agb0[i];
+    delta2[i] = agb2[i] - agb1[i];
+    
+    delta1_pred[i] = a1[spp[i]] + as1[spp[i]]*s[i] + af1[spp[i]]*f[i] + asf1[spp[i]]*sf[i];
+    delta2_pred[i] = a2[spp[i]] + as2[spp[i]]*s[i] + af2[spp[i]]*f[i] + asf2[spp[i]]*sf[i];
+  }
+}
+
+model{	
+  // allometry model
+  b1 ~ lognormal(log(0.5), 0.3);
+  b2 ~ normal(0.7, 0.2);
+  s_allo ~ normal(0, 2);
+  
+  for(i in 1:N_allo){
+    target += normal_lpdf(agb_allo[i] | agb_allo_pred[i], s_allo[spp_allo[i]]);
+  }
+  
+  a1 ~ normal(0, 1);
+  as1 ~ normal(0, 1);
+  af1 ~ normal(0, 1);
+  asf1 ~ normal(0, 1);
+  
+  a2 ~ normal(0, 1);
+  as2 ~ normal(0, 1);
+  af2 ~ normal(0, 1);
+  asf2 ~ normal(0, 1);
+  
+  s_y ~ normal(0, 1);
+  
+  for(i in 1:N){
+    target += normal_lpdf(delta1[i] | delta1_pred[i], s_y[spp[i]]);
+    target += normal_lpdf(delta2[i] | delta2_pred[i], s_y[spp[i]]);
+  }
+}	
